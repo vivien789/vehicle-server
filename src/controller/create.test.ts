4c08f84eb5ed219b0547f41fa10d6620aff77d24
vivien.boucher22 <vivien.boucher22@gmail.com>
2025-11-21 12:40:57 +0000

    lint

---
+++ src/controller/create.test.ts
@@ -0,0 +1,63 @@
+import {expect, jest, test} from '@jest/globals';
+import { Pool } from 'pg';
+import { Request, Response } from 'express';
+
+import { FakeResponse } from "../fake/response";
+import { CreateVehicleController } from "./create";
+import { Vehicle } from "../model/vehicle";
+import { VehicleStore } from "../store/vehicle";
+//import { AppError, ErrorCode } from "../errors";
+
+// On définit ici un module `Mock` ie: tout chargement du module `import { VehicleStore } from "../store/vehicle'`
+// retournera une """fausse""" implémentation qui  n'intéragit pas avec la base de données.
+jest.mock('../store/vehicle', (() => ({
+  VehicleStore: jest.fn().mockImplementation(() => {
+    return {
+      createVehicle: jest.fn().mockImplementation(async (req: any): Promise<Vehicle> => {
+        return new Vehicle(
+          12,
+          req.shortcode,
+          req.battery,
+          req.position,
+        );
+      }),
+    }
+  })
+})));
+
+// Describe décrit un groupe logique de tests, ayant la même logique de mise en place et de nettoyage.
+describe('create vehicle controller', () => {
+  let controller: CreateVehicleController;
+  let store: VehicleStore;
+
+  // Avant chaque test on réinitialise le store et le controller.
+  beforeEach(() => {
+    store =  new VehicleStore({} as Pool); // <- instance mockée!
+    controller = new CreateVehicleController(store);
+  });
+
+  test('creates a valid vehicle', async () => {
+    // Given (mise en place du test).
+    const req = {
+      body: {
+        shortcode: 'abac',
+        battery: 17,
+        longitude: 45,
+        latitude: 45
+      },
+    };
+
+    const resp = new FakeResponse();
+
+    // When.
+    await controller.handle(req as Request, resp as unknown as Response);
+
+    // Then.
+    expect(resp.gotStatus).toEqual(200);
+    expect(resp.gotJson).toEqual({"vehicle": {"battery": 17, "id": 12, "position": {"latitude": 45, "longitude": 45}, "shortcode": "abac"}});
+  });
+
+    // When (exécution de la méthode testée).
+
+    // Then (validation des résultats.
+});
