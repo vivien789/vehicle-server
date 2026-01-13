f2499d4d8d92acb08352b760d5b09c62f9ac91cd
vivien789 <vivien.boucher22@gmail.com>
2026-01-13 12:03:37 +0100

    fix for position option

--- split.js
+++ split.js
@@ -3,14 +3,14 @@ const program = new Command();
 
 
 program
-  .option('--id <number>', 'vehicle id')
-  .option('--sc, --shortcode <string>', 'vehicle code')
-  .option('-b, --battery <number>', 'vehicle battery')
-  .option('-p, --position <Position>', 'vehicle position (lng, lat)')
+  .requiredOption('--id <number>', 'vehicle id')
+  .requiredOption('--sc, --shortcode <string>', 'vehicle code')
+  .requiredOption('-b, --battery <number>', 'vehicle battery')
+  .requiredOption('-p, --position <number...>', 'specify for the vehicle longitude and latitude')
   
-
+program.parse();
 const options = program.opts();
 console.log(options);
 
 //Test
-//node split.js --id 12 --sc 'abc' -b 16 -p {latitude: -70.060316, longitude: 49.432044}
+//node split.js --id 12 --sc 'abc' -b 16 -p -70.060316 49.432044
